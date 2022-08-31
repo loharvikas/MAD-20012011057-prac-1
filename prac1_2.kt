e1fd2016198711ef7caf65a48de53eb8f3978721
loharvikas <56187207+loharvikas@users.noreply.github.com>
2022-08-31 22:32:01 +0530

    Add files via upload

---
+++ prac1_2.kt
@@ -0,0 +1,15 @@
+fun main(){
+    var a:Int = 121
+    var b = a.toDouble()
+    var g: String = "320"
+    var f: String = "233.112"
+    var c = Integer.parseInt(g)
+    var d = g.toInt()
+    var e = f.toDouble()
+    println("Integer Value:$a")
+    println("Double Value From Integer:$b")
+    println("String Value:$g")
+    println("Integer Value1 From String:$c")
+    println("Integer Value2 From String:$d")
+    println("Double Value From String:$e")
+}
